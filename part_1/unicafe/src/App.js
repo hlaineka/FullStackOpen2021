a768f25470c498ffcb113a344a7f3c790ecef6e8
Helvi Lainekallio <helvi.lainekallio@gmail.com>
2021-05-20 18:48:15 +0300

    1.6 done

---
+++ part_1/unicafe/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Display = props => <div>{props.name} {props.value}</div>
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = newValue => {
+    setGood(newValue)
+  }
+
+  const setToNeutral = newValue => {
+    setNeutral(newValue)
+  }
+
+  const setToBad = newValue => {
+    setBad(newValue)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setToGood(good + 1)} text="good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <Display name="good" value={good} />
+      <Display name="neutral" value={neutral} />
+      <Display name="bad" value={bad} />
+    </div>
+  )
+}
+
+export default App

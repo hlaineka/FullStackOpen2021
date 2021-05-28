754fe81f455a4c0268f33ae897364e2de4604c44
Helvi Lainekallio <helvi.lainekallio@gmail.com>
2021-05-28 10:50:16 +0300

    2.14

--- part_2/maat/src/App.js
+++ part_2/maat/src/App.js
@@ -23,15 +23,36 @@ const ShowButton = (props) => (
     </button>
   )
 
-const Country = ({country}) => {
-  console.log(country)
+const Weather = ({api_key, capital}) => {
+  const [ weather, setWeather ] = useState(0)
+  const url = 'http://api.weatherstack.com/current?access_key='+ api_key + '&query=' + capital
+  console.log(url)
+  useEffect(() => {
+    axios.get(url).then(response => {
+    setWeather(response.data.current)
+    })
+  }, [url])
+  console.log("weather:")
+  console.log(weather)
+  return(
+  <div>
+    <p>Temperature: {weather.temperature} celcius</p>
+    <img src={weather.weather_icons} alt="weather icon"/>
+    <p>Wind: {weather.wind_speed} mph direction {weather.wind_dir}</p>
+  </div>
+  )
+}
+
+const Country = ({country, api_key}) => {
   return(
   <div>
     <h1>{country.name}</h1>
     <p>Capital: {country.capital}</p>
     <p>Population: {country.population}</p>
     <Languages languages={country.languages}/>
-    <img src={country.flag} width="300"/>
+    <img src={country.flag} alt="flag" width="300"/>
+    <h3>Weather in {country.capital}</h3>
+    <Weather api_key={api_key} capital={country.capital}/>
   </div>
 )}
 
@@ -40,6 +61,7 @@ function App() {
   const [ countries, setCountries] = useState([])
   const [ selectedCountry, setSelectedCountry] = useState(0)
   const [ filteredCountries, setFilteredCountries] = useState([])
+  const api_key = process.env.REACT_APP_API_KEY
 
   useEffect(() => {
     axios
@@ -58,13 +80,13 @@ function App() {
 
   const CountryList = () => {
     if (selectedCountry)
-      return (<Country country={selectedCountry}/>)
+      return (<Country country={selectedCountry} api_key={api_key}/>)
     if (filteredCountries.length > 10)
       return (<div>Too many matches, specify another filter</div>)
     else if (filteredCountries.length > 1)
       return (<Countries countries={filteredCountries}/>)
     else if (filteredCountries.length === 1)
-      return (<Country country={filteredCountries[0]}/>)
+      return (<Country country={filteredCountries[0]} api_key={api_key}/>)
     else
       return (<p>No countries</p>)
   }
